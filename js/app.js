3db2d9568bab9df8a13c676da156fa0b973b431c
BILLIEBAXTER <hello.billiebaxter@gmail.com>
2023-06-17 00:53:31 +0100

    solving

--- js/app.js
+++ js/app.js
@@ -8,12 +8,15 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function myFunc(a, b) { 
+    let mySum = a + b
+    let message = "the sum of " + 4 + " and " + 7 + " is " + 11 + "."
+return [mySum, message]
 
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -26,7 +29,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a, b) {
+    let myMulti = a * b
+    let message = "the product of"
 
 }
 
@@ -67,11 +72,24 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
-
+function multiply(a, b) {
+    let multiMulti = (a * b);
+    let stringString = "the product of " + a + " and " + b + " is " + multiMulti + "."
+    return [multiMulti, stringString]
 }
+
+function sumAndMultiply(a, b, c) {
+    let sum1 = sum(a, b)[0];
+    let sum2 = sum(sum1, c)[0];
+
+    let string1 = a + " and " + b + " and " + c + " sum to " + multi2 + "."
+    let string2 = "the product of 4 and 7 and 5 is 140."
+    return [sum1, multi2, string1, string2]
+}
+
+sumAndMultiply (2,3,4)
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
